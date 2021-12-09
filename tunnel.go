932f4fc942ac66ff72a2c9a20fb399bf8cf8ee72
michaelskyba <michaelskyba1411@gmail.com>
2021-12-09 16:55:00 -0500

    add sample go file

---
+++ tunnel.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var usage = `Valid commands:
+	tunnel new_cards <deck filename>
+	tunnel due <deck filename>
+	tunnel <front|back> <line number> <deck filename>
+	tunnel review <line number> <score> <deck filename>
+See README.md for more information.`
+
+func main() {
+	fmt.Println(usage)
+}
