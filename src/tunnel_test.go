c88a9e3f4950ecb4fffe9f9718107976edbd8781
Michael Skyba <michaelskyba@net-c.com>
2023-04-30 19:27:24 -0400

    tunnel_test.go: Improve style

--- src/tunnel_test.go
+++ src/tunnel_test.go
@@ -9,18 +9,19 @@ func TestNewCard(tests *testing.T) {
 	}
 
 	cases := []test{
-		test{"", ""},
-		test{"foo", "foo"},
-		test{"	", "		0	2.5	0	1617249600"},
-		test{"foo	", "foo		0	2.5	0	1617249600"},
-		test{"	bar", "	bar	0	2.5	0	1617249600"},
-		test{"		", "		"},
-		test{"		baz", "		baz"},
-		test{"			", "			"},
-		test{"foo	bar", "foo	bar	0	2.5	0	1617249600"},
-		test{"foo	bar	baz", "foo	bar	baz"},
-		test{"foo	bar	0	2.5	0	1617249600", "foo	bar	0	2.5	0	1617249600"},
-		test{"foo	bar	1	2.62	3	1890795600", "foo	bar	1	2.62	3	1890795600"}}
+		{"", ""},
+		{"foo", "foo"},
+		{"	", "		0	2.5	0	1617249600"},
+		{"foo	", "foo		0	2.5	0	1617249600"},
+		{"	bar", "	bar	0	2.5	0	1617249600"},
+		{"		", "		"},
+		{"		baz", "		baz"},
+		{"			", "			"},
+		{"foo	bar", "foo	bar	0	2.5	0	1617249600"},
+		{"foo	bar	baz", "foo	bar	baz"},
+		{"foo	bar	0	2.5	0	1617249600", "foo	bar	0	2.5	0	1617249600"},
+		{"foo	bar	1	2.62	3	1890795600", "foo	bar	1	2.62	3	1890795600"},
+	}
 
 	// Iterate over test cases
 	var got string
